35648baf104cee47d66cc184f6c9ae9d0204cd34
שחר כהן <s9343500@iaf.idf.il>
2025-07-30 17:32:51 +0300

    feat(newEntity): added dropdows inputs and fixed some types

--- src/components/Dynamics/newEntityForm.tsx
+++ src/components/Dynamics/newEntityForm.tsx
@@ -1,53 +1,85 @@
-import {
-    Button,
-    FormControl,
-  } from "@mui/material";
-  
-  import "../../i18n";
-  import { useTranslation } from "react-i18next";
-  
-  import { ThemeProvider, useTheme } from "@mui/material/styles";
-  import rtlPlugin from "@mui/stylis-plugin-rtl";
-  import { prefixer } from "stylis";
-  import { CacheProvider } from "@emotion/react";
-  import createCache from "@emotion/cache";
+import { Button, FormControl } from "@mui/material";
+
+import "../../i18n";
+import { useTranslation } from "react-i18next";
+
+import { ThemeProvider, useTheme } from "@mui/material/styles";
+import rtlPlugin from "@mui/stylis-plugin-rtl";
+import { prefixer } from "stylis";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 import FilterSearchBar from "./filterSearchBar";
-  
-  interface NewEntityProps {
-    textInputs: {string: (fields: string) => void};
-    dropdownInputs: {string: (fields: string) => void};
-    callback: () => void,
-    width?: string;
-  }
-  
-  const cacheRtl = createCache({
-    key: "muirtl",
-    stylisPlugins: [prefixer, rtlPlugin],
-  });
-  
-  const NewEntity: React.FC<NewEntityProps> = ({
-    textInputs, callback, width
-  }) => {
-    const theme = useTheme();
-    const { t } = useTranslation();
-  
-    return (
-      <CacheProvider value={cacheRtl}>
-        <ThemeProvider theme={theme}>
-          <FormControl
+import FilterDropdown from "./filterDropdown";
+
+interface NewEntityProps {
+  textInputs: { label: string; setter: (fields: string) => void }[];
+  dropdownInputs: {
+    label: string;
+    options: string[];
+    selected: string[];
+    setter: (fields: string[]) => void;
+  }[];
+  callback: () => void;
+  width?: string;
+}
+
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
+const NewEntity: React.FC<NewEntityProps> = ({
+  textInputs,
+  dropdownInputs,
+  callback,
+  width,
+}) => {
+  const theme = useTheme();
+  const { t } = useTranslation();
+
+  return (
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <FormControl
+          sx={{
+            width: width,
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          {textInputs.map((textField) => (
+            <FilterSearchBar
+              label={textField.label}
+              setSearch={textField.setter}
+            />
+          ))}
+          {dropdownInputs.map((dropdown) => (
+            <FilterDropdown
+              label={dropdown.label}
+              options={dropdown.options}
+              selected={dropdown.selected}
+              setSelected={dropdown.setter}
+              isMultiple={true}
+              width="9rem"
+            />
+          ))}
+          <Button
             sx={{
-              width: width,
-              display: "flex",
-              flexDirection: "row"
+              color: "#000000",
+              bgcolor: "#ffffff",
+              borderRadius: 2,
+              ":hover": { bgcolor: "#d4edff7a" },
+            }}
+            onClick={() => {
+              callback();
             }}
           >
-            {Object.keys(textInputs).map((text) => (<FilterSearchBar label={text} setSearch={textInputs[text]}/>))}
-            <Button sx={{ color: "#000000", bgcolor: "#ffffff", borderRadius: 2, ":hover": {bgcolor: "#d4edff7a"} }} onClick={() => {callback()}}>{t("add")}</Button>
-          </FormControl>
-        </ThemeProvider>
-      </CacheProvider>
-    );
-  };
-  
-  export default NewEntity;
-  
+            {t("add")}
+          </Button>
+        </FormControl>
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default NewEntity;
