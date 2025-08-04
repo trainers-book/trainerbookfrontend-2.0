32b2dd17a5eadd1fe0a670c427fbad72e93e9678
שחר כהן <s9343500@iaf.idf.il>
2025-08-04 11:34:44 +0300

    fix(login): refactor page to be a form that can be submitted using enter key and not just the button

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../context/UserContext";
+import { useTranslation } from "react-i18next";
 import "../assets/fonts/Alef-Regular.ttf";
 import "../style/login.css";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 const LoginPage = () => {
+  const { t } = useTranslation();
   const [username, setUsernameInput] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
@@ -17,27 +20,58 @@ const LoginPage = () => {
       setUsername(username);
       navigate("/reviewFlights");
     } else {
-      alert("שם משתמש או סיסמה שגויים");
+      alert(t("wrongLogin"));
     }
   };
 
   return (
-    <div className="login-container">
-      <h2>התחברות</h2>
-      <input
-        type="text"
-        placeholder="שם משתמש"
-        value={username}
+    <Box
+      component={"form"}
+      onSubmit={handleLogin}
+      sx={{
+        bgcolor: "#ffffff",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        m: "auto",
+        mt: "10vh",
+        maxWidth: "22vw",
+        borderRadius: 3,
+        boxShadow: "0 0 15px rgba(0, 0, 0, 0.1)"
+      }}
+    >
+      <Typography sx={{ fontSize: "2rem", m: 2, mb: 4, fontWeight: "bold" }} component="h2">
+        {t("login")}
+      </Typography>
+      <TextField
+        sx={{
+          mb: 2,
+          borderRadius: 3,
+          "& .MuiOutlinedInput-root": {
+            borderRadius: 3, 
+          },
+          width: "17vw"
+        }}
+        placeholder={t("userName")}
         onChange={(e) => setUsernameInput(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="סיסמה"
-        value={password}
+      ></TextField>
+      <TextField
+        sx={{
+          mb: 2,
+          borderRadius: 3,
+          "& .MuiOutlinedInput-root": {
+            borderRadius: 3, 
+          },
+          width: "17vw"
+        }}
+        placeholder={t("password")}
         onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>התחבר</button>
-    </div>
+        type="password"
+      ></TextField>
+      <Button sx={{ bgcolor: "#6499ff", color: "#ffffff", width: "17vw", borderRadius: 3, mb: 2 }} type="submit">
+        {t("connect")}
+      </Button>
+    </Box>
   );
 };
 
