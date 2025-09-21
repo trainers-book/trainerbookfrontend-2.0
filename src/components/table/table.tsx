fcc997cba9a4b538b5e26833c94aee81936ba9d9
שחר כהן <s9343500@iaf.idf.il>
2025-09-21 17:01:18 +0300

    removed comments

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,6 +1,6 @@
 ﻿import "./table.css";
 import "../../i18n";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   TableContainer,
   TableHead,
@@ -18,9 +18,6 @@ import { Status } from "../../types/statuses";
 import IssueData from "../../types/tables/issues";
 import { Severity } from "../../types/issuesSeverity";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { useBackend } from "../../context/backendContext";
-import { usePlatforms } from "../../context/platformsContext";
-import { HttpStatusCode } from "axios";
 
 interface TableProps {
   properties: string[];
@@ -47,8 +44,6 @@ const GenericTable: React.FC<TableProps> = ({
 }) => {
   const tableHeightPercent = 85;
   const tableRowHeight = 82;
-  const tableFetchExtra = 4;
-  const tableHeadHeight = 56.5;
 
   const { t } = useTranslation();
   const columns = properties.slice();
@@ -58,81 +53,6 @@ const GenericTable: React.FC<TableProps> = ({
   if (deleteRow != undefined) {
     columns.push("!delete");
   }
-
-  // const [offset, setOffset] = useState(0);
-  // const [sortedData, setSortedData] = useState(data.sort(sortFunction));
-  // const limit = Math.round(
-  //   (window.innerHeight * (tableHeightPercent / 100)) / tableRowHeight +
-  //     tableFetchExtra
-  // );
-  // const [dataToShow, setdataToShow] = useState(
-  //   sortedData.slice(offset, limit * offset)
-  // );
-  // const [dataToShow, setdataToShow] = useState([]);
-
-  // useEffect(() => {
-  //   fetchMoreData();
-  // }, [offset]);
-
-  // useEffect(() => {
-  //   setOffset(0);
-  //   const sorted = data.sort(sortFunction);
-  //   setSortedData(sorted);
-  //   setdataToShow(sorted.slice(0, limit));
-  // }, [data]);
-
-  // useEffect(() => {
-  //   fetchMoreData();
-  // }, [platforms]);
-
-  // const fetchMoreData = async () => {
-  //   if (platforms.length == 0) {
-  //     return;
-  //   }
-
-  //   const newData = await connection.getObjects(
-  //     fetchCollection,
-  //     offset * 25,
-  //     platforms
-  //   );
-  //   console.log(newData);
-
-  //   // const newData = sortedData.slice(offset * limit, (offset + 1) * limit);
-  //   if (newData.status == HttpStatusCode.Ok) {
-  //     setdataToShow([
-  //       ...new Set([
-  //         ...dataToShow,
-  //         ...(newData.data.map(
-  //           (malf) =>
-  //             new IssueData(
-  //               new Date(malf.date),
-  //               malf._id,
-  //               malf.failureDetails,
-  //               malf.malfunctionOpener,
-  //               malf.failureDetails,
-  //               malf.platform,
-  //               malf.disruption,
-  //               malf.failureStatus
-  //             )
-  //         ).sort(sortFunction)),
-  //       ]),
-  //     ]);
-  //   }
-  // };
-
-  // const handleScroll = (event: any) => {
-  //   const children = document.getElementById("table")?.childElementCount;
-  //   const target = event.target;
-
-  //   if (
-  //     target.scrollTop + target.offsetHeight + tableHeadHeight >=
-  //     tableRowHeight * (children ? children : 1)
-  //   ) {
-  //     setTimeout(() => {
-  //       setOffset(offset + 1);
-  //     }, 100); // timeout to simulate fetch time from server
-  //   }
-  // };
 
   const valueToMultipleLines = (
     value: Date | string | number | Status | Severity
