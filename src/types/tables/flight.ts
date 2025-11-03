f00c10e6df1656ba212941ac236d4e801c5ba6eb
שחר כהן <s9343500@iaf.idf.il>
2025-11-03 16:30:36 +0200

    changed and added types properties

--- src/types/tables/flight.ts
+++ src/types/tables/flight.ts
@@ -1,38 +1,70 @@
+import { iafWeekFormat } from "../../common/iafWeek";
+
 export default class FlightData {
   dateTime: Date;
   date: string;
   flightNumber: number;
   flightName: string;
   instructorName: string;
-  observer: string;
-  airCrew1: string;
-  airCrew2: string;
-  issueDescription: string;
+  _observer: string;
   startTime: string;
   flightTime: number;
   platform: string;
+  _malfNumbers: number[];
+  _airCrew1: string | undefined;
+  _airCrew2: string | undefined;
+  _block: string | undefined;
+  _disruption: string | boolean | undefined;
+  _navigator: string | undefined;
+  _pilot: string | undefined;
+  _technician: string | undefined;
+  _timeOffFlight: string | undefined;
+  _130: string | undefined;
+  _131: string | undefined;
+  _132: string | undefined;
+  _133: string | undefined;
+  _140: string | undefined;
+  _141: string | undefined;
+  _142: string | undefined;
+  _143: string | undefined;
+  _configuration: string | undefined;
+  _inspectorInstructor: string | undefined;
+  _iafWeek: number;
 
   constructor(
-    dateTime = new Date(),
+    {dateTime = new Date(),
     flightNumber = 0,
     flightName = "",
     instructorName = "",
     observer = "",
-    airCrew1 = "",
-    airCrew2 = "",
-    issueDescription = "",
     flightTime = 0,
-    platform = ""
+    platform = "",
+    malfNumbers = [],
+    airCrew1 = undefined,
+    airCrew2 = undefined,
+    block = undefined,
+    disruption = undefined,
+    navigator = undefined,
+    pilot = undefined,
+    technician = undefined,
+    timeOffFlight = undefined,
+    _130 = undefined,
+    _131 = undefined,
+    _132 = undefined,
+    _133 = undefined,
+    _140 = undefined,
+    _141 = undefined,
+    _142 = undefined,
+    _143 = undefined,
+    configuration = undefined,
+    inspectorInstructor = undefined}
   ) {
     this.dateTime = dateTime;
     this.date = dateTime.toLocaleDateString("en-GB");
     this.flightNumber = flightNumber;
     this.flightName = flightName;
     this.instructorName = instructorName;
-    this.observer = observer;
-    this.airCrew1 = airCrew1;
-    this.airCrew2 = airCrew2;
-    this.issueDescription = issueDescription;
+    this._observer = observer;
     this.startTime = dateTime.toLocaleTimeString([], {
       hour12: false,
       hour: "2-digit",
@@ -40,5 +72,27 @@ export default class FlightData {
     });
     this.flightTime = flightTime;
     this.platform = platform;
+    this._malfNumbers = malfNumbers;
+    this._airCrew1 = airCrew1;
+    this._airCrew2 = airCrew2;
+    this._block = block;
+    this._disruption = disruption;
+    this._navigator = navigator;
+    this._pilot = pilot;
+    this._technician = technician;
+    this._timeOffFlight = timeOffFlight;
+    this._130 = _130;
+    this._131 = _131;
+    this._132 = _132;
+    this._133 = _133;
+    this._140 = _140;
+    this._141 = _141;
+    this._142 = _142;
+    this._143 = _143;
+    this._configuration = configuration;
+    this._inspectorInstructor = inspectorInstructor;
+    this._iafWeek = iafWeekFormat(this.dateTime);
+    console.log(this._iafWeek);
+    
   }
 }
