32c2f33a30020abdb42620213d0d06f71847a5ba
s9181048 <s9181048@iaf.idf.il>
2026-01-12 16:14:29 +0200

    fix tabs

--- src/common/iafWeek.ts
+++ src/common/iafWeek.ts
@@ -1,17 +1,17 @@
 export const iafWeekFormat = (date: Date) => {
-    const day = 24 * 60 * 60 * 1000;
-    const currentTime = date;
-    const firstDayOfYear = new Date(currentTime.getFullYear(), 0, 1);
-    const dayOfWeek = firstDayOfYear.getDay(); 
-    const weekOneStart = new Date(firstDayOfYear);
-  
-    weekOneStart.setDate(firstDayOfYear.getDate() - dayOfWeek);
-  
-    const daysSinceSunday = Math.floor(
-      (currentTime.getTime() - weekOneStart.getTime()) / day
-    );
-  
-    let week = Math.floor(daysSinceSunday / 7) + 1;
-  
-    return week > 52 ? ((week - 1) % 52) + 1 : week
-  };
+  const day = 24 * 60 * 60 * 1000;
+  const currentTime = date;
+  const firstDayOfYear = new Date(currentTime.getFullYear(), 0, 1);
+  const dayOfWeek = firstDayOfYear.getDay();
+  const weekOneStart = new Date(firstDayOfYear);
+
+  weekOneStart.setDate(firstDayOfYear.getDate() - dayOfWeek);
+
+  const daysSinceSunday = Math.floor(
+    (currentTime.getTime() - weekOneStart.getTime()) / day
+  );
+
+  let week = Math.floor(daysSinceSunday / 7) + 1;
+
+  return week > 52 ? ((week - 1) % 52) + 1 : week;
+};
