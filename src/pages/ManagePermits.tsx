55793a02f40627a5798f4c83289a0c99eb3d3dfe
Orian Roth <orianrot@gmail.com>
2026-06-06 22:27:23 +0300

    Add permit information popup

--- src/pages/ManagePermits.tsx
+++ src/pages/ManagePermits.tsx
@@ -8,6 +8,7 @@ import InfinateScrollFetch from "../components/table/infinateScrollTableFetch";
 import PermitData, { getPermitColor, PermitObjectFromFetch } from "../types/tables/permits";
 import FilterPermits from "../components/filterTables/filterPermits";
 import { PermitStatus } from "../types/statuses";
+import PermitInformation from "../components/Popup/information/permitInformation";
 
 const ManagePermits: React.FC = () => {
   const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>([]);
@@ -16,6 +17,10 @@ const ManagePermits: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState<
       { minDate: Date; maxDate: Date } | undefined
     >(undefined);
+  const [selectedPermitPopup, setSelectedPermitPopup] = useState<
+    PermitData | undefined
+  >();
+  const [updatedPermit, setUpdatedPermit] = useState<PermitData | undefined>();
   const { platforms } = usePlatforms();
 
   const getPlatformsAndFilters = () => {
@@ -60,9 +65,13 @@ const ManagePermits: React.FC = () => {
         color={true}
         objectFromFetch={PermitObjectFromFetch}
         platformsAndFilters={getPlatformsAndFilters()}
+        externalUpdate={updatedPermit}
+        clickable={(row: PermitData) => {
+          setSelectedPermitPopup(row);
+        }}
       />
     );
-  }, [selectedPlatforms, selectedStatuses, selectedDate]);
+  }, [selectedPlatforms, selectedStatuses, selectedDate, updatedPermit]);
 
   return (
     <PageWrapper>
@@ -91,6 +100,17 @@ const ManagePermits: React.FC = () => {
         <NewPermitModel />
       </Box>
       {memoTable}
+      {selectedPermitPopup && (
+        <PermitInformation
+          isOpen={selectedPermitPopup !== undefined}
+          selectedRow={selectedPermitPopup}
+          onClose={() => setSelectedPermitPopup(undefined)}
+          onSave={(permit) => {
+            setSelectedPermitPopup(permit);
+            setUpdatedPermit(permit);
+          }}
+        />
+      )}
     </PageWrapper>
   );
 };
